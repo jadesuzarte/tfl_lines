e609c795655f6484c2e6b047eaa5869a4b920bb9
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-03-03 15:18:24 +0000

    update main js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 
 const api_key = '491c623c555e291b2dbb5d11d2ab1985';
+const app_id = '2cfcbdcd'
+const url = 'https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=2cfcbdcd&app_key=491c623c555e291b2dbb5d11d2ab1985'
 
+const lines = []; 
+
+fetch
