7c85c133b36512199628d975c6ec132eb1e32692
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-03-03 16:15:04 +0000

    get lines names and statuses to display

--- main.js
+++ main.js
@@ -5,4 +5,22 @@ const url = 'https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=2cf
 
 const lines = []; 
 
-fetch
+fetch(url)
+.then(blob => blob.json())
+.then(data => lines.push(...data))
+
+function displayLines() {
+const html = lines.map(line =>  {
+    return `
+    <li> 
+    <span class ="name"> ${line.name}, ${line.lineStatuses[0].statusSeverityDescription}</span>
+     <span class="pupulation">${line.serviceTypes.name}</span>
+    </li>
+     `;
+}).join('')
+allLines.innerHTML = html
+}
+
+const allLines = document.querySelector('.allLines')
+
+allLines.addEventListener('click', displayLines);
