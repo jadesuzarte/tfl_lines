8aab51124ad9688f8cfe452599fe7885545dac6d
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-03-04 19:28:55 +0000

    change comas to arrows in html

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 function displayLines() {
-    const api_key = '491c623c555e291b2dbb5d11d2ab1985';
+const api_key = '491c623c555e291b2dbb5d11d2ab1985';
 const app_id = '2cfcbdcd'
 const url = 'https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=2cfcbdcd&app_key=491c623c555e291b2dbb5d11d2ab1985'
 const lines = []; 
@@ -17,7 +17,7 @@ fetch(url)
     html = lines.map(line =>  {
         return `
         <p> 
-        <span class ="lines"> ${line.name}, ${line.lineStatuses[0].statusSeverityDescription}</span> <br>
+        <span class ="lines"> ${line.name} -> ${line.lineStatuses[0].statusSeverityDescription}</span> <br>
         </p>
          `;
     }).join('')
