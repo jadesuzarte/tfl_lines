8f61adf1e53b1d140bfc19e30d6ae53c6fa3ddf7
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-03-04 19:17:10 +0000

    delete button and make data show onload

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
 
-const api_key = '491c623c555e291b2dbb5d11d2ab1985';
-const app_id = '2cfcbdcd'
-const url = 'https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=2cfcbdcd&app_key=491c623c555e291b2dbb5d11d2ab1985'
-
-const lines = []; 
-
-fetch(url)
-.then(blob => blob.json())
-.then(data => lines.push(...data))
 
 function displayLines() {
-const html = lines.map(line =>  {
-    return `
-    <p> 
-    <span class ="lines"> ${line.name}, ${line.lineStatuses[0].statusSeverityDescription}</span> <br>
-    </p>
-     `;
-}).join('')
-allLines.innerHTML = html
+    const api_key = '491c623c555e291b2dbb5d11d2ab1985';
+const app_id = '2cfcbdcd'
+const url = 'https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=true&app_id=2cfcbdcd&app_key=491c623c555e291b2dbb5d11d2ab1985'
+const lines = []; 
+let html = ""
+
+//page was loading and reading allLines.innerHtml before fetch could be completed, so I moved fetch inside the function 
+fetch(url)
+.then(blob => blob.json())
+.then(data => {
+    console.log(data)
+    lines.push(...data)
+
+    html = lines.map(line =>  {
+        return `
+        <p> 
+        <span class ="lines"> ${line.name}, ${line.lineStatuses[0].statusSeverityDescription}</span> <br>
+        </p>
+         `;
+    }).join('')
+
+    allLines.innerHTML = html
+})
+
+console.log(lines.length);
 }
 
-window.onload = displayLines;
+window.onload = function() {
+    displayLines()
+};
 
 const allLines = document.querySelector('.allLines')
-const button = document.querySelector('.button')
-button.addEventListener('click', displayLines);
